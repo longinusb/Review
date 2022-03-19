d722fa035d8df770ed9f3ac760d3a1b95c5e11e6
longinusb <512118005@qq.com>
2022-03-20 00:34:41 +0800

    5 commit

---
+++ ES6^/Module/export-default.js
@@ -0,0 +1,4 @@
+//默认输出
+export default function () {
+  console.log("foo");
+}
---
+++ ES6^/Module/main.js
@@ -0,0 +1,10 @@
+import { area, circumference } from "./circle.js";
+//整体加载
+// import * as circle from './circle'
+
+console.log("圆面积" + area(4));
+console.log("圆周长" + circumference(14));
+
+//加载默认输出
+import customName from "./circle";
+customName();
