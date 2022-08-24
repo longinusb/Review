dffe4d02c1a3c990c002bed85f53b122eb9a17ba
longinusb <512118005@qq.com>
2022-08-24 15:30:23 +0800

    my.vue

--- my.vue
+++ my.vue
@@ -6,27 +6,11 @@
       </el-table-column>
       <el-table-column prop="id" label="序号" width="200">
         <template slot-scope="scope">
-          <el-input v-model="scope.row.id"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column prop="date" label="环节" width="180">
-        <template slot-scope="scope">
-          <el-input v-model="scope.row.date"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column prop="name" label="动作" width="180">
-        <template slot-scope="scope">
-          <el-input v-model="scope.row.name"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column prop="address" label="维度">
-        <template slot-scope="scope">
-          <el-input v-model="scope.row.address"></el-input>
-        </template>
-      </el-table-column>
-      <el-table-column prop="address" label="备注">
-        <template slot-scope="scope">
-          <el-input v-model="scope.row.address"></el-input>
+          <span v-show="!scope.row.editable">{{ scope.row.id }}</span>
+          <el-input
+            v-show="scope.row.editable"
+            v-model="scope.row.id"
+          ></el-input>
         </template>
       </el-table-column>
       <el-table-column fixed="right" label="处理流程">
@@ -65,24 +49,26 @@ export default {
   },
   data() {
     return {
-      indexList: [], //序号数组,
       tableData: [],
     };
   },
   methods: {
-    handleClickParent() {
-      if (this.tableData == undefined) {
-        this.tableData = new Array();
-      }
+    newobj(idx) {
       let obj = {
-        idx: this.tableData.length + 1,
+        idx: idx,
         id: "",
         date: "",
         name: "",
         address: "",
+        children: null,
+        editable: false,
       };
-
-      this.tableData.push(obj);
+    },
+    handleClickParent() {
+      if (this.tableData == undefined) {
+        this.tableData = new Array();
+      }
+      this.tableData.push(this.newobj(this.tableData.length + 1));
     },
     handleClickRow(row) {
       let idx = 1;
@@ -91,40 +77,19 @@ export default {
       } else {
         idx = row.children.length + 1;
       }
-      let obj = {
-        idx: row.idx + "." + idx,
-        id: "",
-        date: "",
-        name: "",
-        address: "",
-      };
-      row.children.push(obj);
+      row.children.push(this.newobj(row.idx + "." + idx));
     },
     handleClickUpdate(row) {
-      console.log(row.editable);
-      row.editable = true;
+      row.editable = !row.editable;
     },
     getList() {
       this.tableData = [
-        {
-          id: 1,
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-          children: null,
-        },
-        {
-          id: 2,
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄",
-          children: null,
-        },
         {
           id: 3,
           date: "2016-05-01",
           name: "王小虎",
           address: "上海市普陀区金沙江路 1519 弄",
+          editable: false,
           children: [
             {
               id: 31,
@@ -132,6 +97,7 @@ export default {
               name: "王小虎",
               address: "上海市普陀区金沙江路 1519 弄",
               children: null,
+              editable: false,
             },
             {
               id: 32,
@@ -139,15 +105,9 @@ export default {
               name: "王小虎",
               address: "上海市普陀区金沙江路 1519 弄",
               children: null,
+              editable: false,
             },
           ],
-        },
-        {
-          id: 4,
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄",
-          children: null,
         },
       ];
       const getIndex = (data, index, parent) => {
